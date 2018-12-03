dd8d561d33fefd966611d5509ab563329976b747
王宁 <>
2018-12-03 16:22:43 +0800

    no message

---
+++ SSIDCard-master/Example/SSIDCard/SSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSAppDelegate.h
+//  SSIDCard
+//
+//  Created by 张家铭 on 2018/4/26.
+//  Copyright © 2018年 sansansisi. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
